4977065e2743149eb1c3d3482e3552d2287f2cca
jsorianoalvarez <jorge.soriano@vinneren.com.mx>
2022-08-20 01:32:58 -0500

    actualizacion en formulario

--- src/work-with-us/index.js
+++ src/work-with-us/index.js
@@ -69,168 +69,326 @@ const WorkWithUs = () => {
             })}
           </h4>
           <form onSubmit={handleSubmit(onSubmit)}>
-            <div className="form-inputs">
-              <div className="form-left">
-                <Label for="position">
-                  {intl.formatMessage({
-                    id: 'app.pages.work-with-us.label.position',
-                  })}
-                </Label>
-                <select
-                  className="select-jobs"
-                  id="position"
-                  name="requiredPosition"
-                  placeholder="Selecciona una opción"
-                  {...register('requiredPosition', { required: true })}
-                >
-                  {jobsObj.map(({ name }, i) => {
-                    return (
-                      <>
-                        <option key={i} value={name}>
-                          {name}
-                        </option>
-                      </>
-                    )
-                  })}
-                </select>
-                {errors.requiredPosition && (
-                  <span style={{ color: '#F00' }}>
+            {window.screen.width > 768 ? (
+              <div className="form-inputs">
+                <div className="form-left">
+                  <Label for="position">
                     {intl.formatMessage({
-                      id: 'app.pages.contactUs.label.input-required',
+                      id: 'app.pages.work-with-us.label.position',
                     })}
-                  </span>
-                )}
-                <Label for="email">
-                  {intl.formatMessage({
-                    id: 'app.pages.contactUs.label.email',
-                  })}
-                </Label>
-                <input
-                  className="form-input"
-                  id="email"
-                  name="email"
-                  {...register('email', { required: true })}
-                  placeholder={intl.formatMessage({
-                    id: 'app.pages.contactUs.label.email',
-                  })}
-                  type="email"
-                  required
-                />
-                {errors.email && (
-                  <span style={{ color: '#F00' }}>
+                  </Label>
+                  <select
+                    className="select-jobs"
+                    id="position"
+                    name="requiredPosition"
+                    placeholder="Selecciona una opción"
+                    {...register('requiredPosition', { required: true })}
+                  >
+                    {jobsObj.map(({ name }, i) => {
+                      return (
+                        <>
+                          <option key={i} value={name}>
+                            {name}
+                          </option>
+                        </>
+                      )
+                    })}
+                  </select>
+                  {errors.requiredPosition && (
+                    <span style={{ color: '#F00' }}>
+                      {intl.formatMessage({
+                        id: 'app.pages.contactUs.label.input-required',
+                      })}
+                    </span>
+                  )}
+                  <Label for="email">
                     {intl.formatMessage({
-                      id: 'app.pages.contactUs.label.input-required',
+                      id: 'app.pages.contactUs.label.email',
                     })}
-                  </span>
-                )}
-                <Label for="linkedIn">
-                  {intl.formatMessage({
-                    id: 'app.pages.work-with-us.label.linkedIn',
-                  })}
-                </Label>
-                <input
-                  className="form-input"
-                  id="linkedIn"
-                  name="linkedIn"
-                  placeholder={intl.formatMessage({
-                    id: 'app.pages.work-with-us.label.linkedIn',
-                  })}
-                  type="text"
-                  {...register('linkedIn', { required: true })}
-                  required
-                />
-                {errors.linkedIn && (
-                  <span style={{ color: '#F00' }}>
-                    {intl.formatMessage({
-                      id: 'app.pages.contactUs.label.input-required',
-                    })}
-                  </span>
-                )}
-              </div>
-              <div className="form-right">
-                <Label for="name">
-                  {intl.formatMessage({
-                    id: 'app.pages.work-with-us.label.name-complete',
-                  })}
-                </Label>
-                <input
-                  className="form-input"
-                  id="name"
-                  name="fullName"
-                  placeholder="Nombre*"
-                  type="text"
-                  {...register('fullName', { required: true })}
-                />
-                {errors.fullName && (
-                  <span style={{ color: '#F00' }}>
-                    {intl.formatMessage({
-                      id: 'app.pages.contactUs.label.input-required',
-                    })}
-                  </span>
-                )}
-                <Label for="telphone">
-                  {intl.formatMessage({
-                    id: 'app.pages.contactUs.label.phone',
-                  })}
-                </Label>
-                <PhoneInput
-                  international
-                  name="phoneNumber"
-                  id="telphone"
-                  placeholder={intl.formatMessage({
-                    id: 'app.pages.contactUs.label.phone',
-                  })}
-                  defaultCountry="MX"
-                  onChange={SetValue}
-                  value={value}
-                  {...register('phoneNumber', { required: true })}
-                />
-                {errors.phoneNumber && (
-                  <span style={{ color: '#F00' }}>
-                    {intl.formatMessage({
-                      id: 'app.pages.contactUs.label.input-required',
-                    })}
-                  </span>
-                )}
-                <div className="container-input">
+                  </Label>
                   <input
-                    id="cvFile"
-                    className="inputfile inputfile-1"
-                    name="cvFile"
-                    type="file"
-                    {...register('cvFile')}
-                    onChangeCapture={setValueFile}
+                    className="form-input"
+                    id="email"
+                    name="email"
+                    {...register('email', { required: true })}
+                    placeholder={intl.formatMessage({
+                      id: 'app.pages.contactUs.label.email',
+                    })}
+                    type="email"
+                    required
                   />
-                  <label htmlFor="cvFile">
-                    {valueFile?.target?.files[0]?.name || (
-                      <>
-                        <svg
-                          xmlns="http://www.w3.org/2000/svg"
-                          className="iborrainputfile"
-                          width="20"
-                          height="17"
-                          viewBox="0 0 20 17"
-                        >
-                          <path d="M10 0l-5.2 4.9h3.3v5.1h3.8v-5.1h3.3l-5.2-4.9zm9.3 11.5l-3.2-2.1h-2l3.4 2.6h-3.5c-.1 0-.2.1-.2.1l-.8 2.3h-6l-.8-2.2c-.1-.1-.1-.2-.2-.2h-3.6l3.4-2.6h-2l-3.2 2.1c-.4.3-.7 1-.6 1.5l.6 3.1c.1.5.7.9 1.2.9h16.3c.6 0 1.1-.4 1.3-.9l.6-3.1c.1-.5-.2-1.2-.7-1.5z"></path>
-                        </svg>
-                        <span className="iborrainputfile">
-                          {intl.formatMessage({
-                            id: 'app.pages.work-with-us.label.select-file',
-                          })}
-                        </span>
-                      </>
-                    )}
-                  </label>
+                  {errors.email && (
+                    <span style={{ color: '#F00' }}>
+                      {intl.formatMessage({
+                        id: 'app.pages.contactUs.label.input-required',
+                      })}
+                    </span>
+                  )}
+                  <Label for="linkedIn">
+                    {intl.formatMessage({
+                      id: 'app.pages.work-with-us.label.linkedIn',
+                    })}
+                  </Label>
+                  <input
+                    className="form-input"
+                    id="linkedIn"
+                    name="linkedIn"
+                    placeholder={intl.formatMessage({
+                      id: 'app.pages.work-with-us.label.linkedIn',
+                    })}
+                    type="text"
+                    {...register('linkedIn', { required: true })}
+                    required
+                  />
+                  {errors.linkedIn && (
+                    <span style={{ color: '#F00' }}>
+                      {intl.formatMessage({
+                        id: 'app.pages.contactUs.label.input-required',
+                      })}
+                    </span>
+                  )}
+                </div>
+                <div className="form-right">
+                  <Label for="name">
+                    {intl.formatMessage({
+                      id: 'app.pages.work-with-us.label.name-complete',
+                    })}
+                  </Label>
+                  <input
+                    className="form-input"
+                    id="name"
+                    name="fullName"
+                    placeholder="Nombre*"
+                    type="text"
+                    {...register('fullName', { required: true })}
+                  />
+                  {errors.fullName && (
+                    <span style={{ color: '#F00' }}>
+                      {intl.formatMessage({
+                        id: 'app.pages.contactUs.label.input-required',
+                      })}
+                    </span>
+                  )}
+                  <Label for="telphone">
+                    {intl.formatMessage({
+                      id: 'app.pages.contactUs.label.phone',
+                    })}
+                  </Label>
+                  <PhoneInput
+                    international
+                    name="phoneNumber"
+                    id="telphone"
+                    placeholder={intl.formatMessage({
+                      id: 'app.pages.contactUs.label.phone',
+                    })}
+                    defaultCountry="MX"
+                    onChange={SetValue}
+                    value={value}
+                    {...register('phoneNumber', { required: true })}
+                  />
+                  {errors.phoneNumber && (
+                    <span style={{ color: '#F00' }}>
+                      {intl.formatMessage({
+                        id: 'app.pages.contactUs.label.input-required',
+                      })}
+                    </span>
+                  )}
+                  <div className="container-input">
+                    <input
+                      id="cvFile"
+                      className="inputfile inputfile-1"
+                      name="cvFile"
+                      type="file"
+                      {...register('cvFile')}
+                      onChangeCapture={setValueFile}
+                    />
+                    <label htmlFor="cvFile">
+                      {valueFile?.target?.files[0]?.name || (
+                        <>
+                          <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            className="iborrainputfile"
+                            width="20"
+                            height="17"
+                            viewBox="0 0 20 17"
+                          >
+                            <path d="M10 0l-5.2 4.9h3.3v5.1h3.8v-5.1h3.3l-5.2-4.9zm9.3 11.5l-3.2-2.1h-2l3.4 2.6h-3.5c-.1 0-.2.1-.2.1l-.8 2.3h-6l-.8-2.2c-.1-.1-.1-.2-.2-.2h-3.6l3.4-2.6h-2l-3.2 2.1c-.4.3-.7 1-.6 1.5l.6 3.1c.1.5.7.9 1.2.9h16.3c.6 0 1.1-.4 1.3-.9l.6-3.1c.1-.5-.2-1.2-.7-1.5z"></path>
+                          </svg>
+                          <span className="iborrainputfile">
+                            {intl.formatMessage({
+                              id: 'app.pages.work-with-us.label.select-file',
+                            })}
+                          </span>
+                        </>
+                      )}
+                    </label>
+                  </div>
                 </div>
               </div>
-            </div>
+            ) : (
+              <div className="form-inputs">
+                <div className="form-left">
+                  <Label for="position">
+                    {intl.formatMessage({
+                      id: 'app.pages.work-with-us.label.position',
+                    })}
+                  </Label>
+                  <select
+                    className="select-jobs"
+                    id="position"
+                    name="requiredPosition"
+                    placeholder="Selecciona una opción"
+                    {...register('requiredPosition', { required: true })}
+                  >
+                    {jobsObj.map(({ name }, i) => {
+                      return (
+                        <>
+                          <option key={i} value={name}>
+                            {name}
+                          </option>
+                        </>
+                      )
+                    })}
+                  </select>
+                  {errors.requiredPosition && (
+                    <span style={{ color: '#F00' }}>
+                      {intl.formatMessage({
+                        id: 'app.pages.contactUs.label.input-required',
+                      })}
+                    </span>
+                  )}
+                  <Label for="name">
+                    {intl.formatMessage({
+                      id: 'app.pages.work-with-us.label.name-complete',
+                    })}
+                  </Label>
+                  <input
+                    className="form-input"
+                    id="name"
+                    name="fullName"
+                    placeholder="Nombre*"
+                    type="text"
+                    {...register('fullName', { required: true })}
+                  />
+                  {errors.fullName && (
+                    <span style={{ color: '#F00' }}>
+                      {intl.formatMessage({
+                        id: 'app.pages.contactUs.label.input-required',
+                      })}
+                    </span>
+                  )}
+                  <Label for="email">
+                    {intl.formatMessage({
+                      id: 'app.pages.contactUs.label.email',
+                    })}
+                  </Label>
+                  <input
+                    className="form-input"
+                    id="email"
+                    name="email"
+                    {...register('email', { required: true })}
+                    placeholder={intl.formatMessage({
+                      id: 'app.pages.contactUs.label.email',
+                    })}
+                    type="email"
+                    required
+                  />
+                  {errors.email && (
+                    <span style={{ color: '#F00' }}>
+                      {intl.formatMessage({
+                        id: 'app.pages.contactUs.label.input-required',
+                      })}
+                    </span>
+                  )}
+                </div>
+                <div className="form-right">
+                  <Label for="telphone">
+                    {intl.formatMessage({
+                      id: 'app.pages.contactUs.label.phone',
+                    })}
+                  </Label>
+                  <PhoneInput
+                    international
+                    name="phoneNumber"
+                    id="telphone"
+                    placeholder={intl.formatMessage({
+                      id: 'app.pages.contactUs.label.phone',
+                    })}
+                    defaultCountry="MX"
+                    onChange={SetValue}
+                    value={value}
+                    {...register('phoneNumber', { required: true })}
+                  />
+                  {errors.phoneNumber && (
+                    <span style={{ color: '#F00' }}>
+                      {intl.formatMessage({
+                        id: 'app.pages.contactUs.label.input-required',
+                      })}
+                    </span>
+                  )}
+                  <Label for="linkedIn">
+                    {intl.formatMessage({
+                      id: 'app.pages.work-with-us.label.linkedIn',
+                    })}
+                  </Label>
+                  <input
+                    className="form-input"
+                    id="linkedIn"
+                    name="linkedIn"
+                    placeholder={intl.formatMessage({
+                      id: 'app.pages.work-with-us.label.linkedIn',
+                    })}
+                    type="text"
+                    {...register('linkedIn', { required: true })}
+                    required
+                  />
+                  {errors.linkedIn && (
+                    <span style={{ color: '#F00' }}>
+                      {intl.formatMessage({
+                        id: 'app.pages.contactUs.label.input-required',
+                      })}
+                    </span>
+                  )}
+                  <div className="container-input">
+                    <input
+                      id="cvFile"
+                      className="inputfile inputfile-1"
+                      name="cvFile"
+                      type="file"
+                      {...register('cvFile')}
+                      onChangeCapture={setValueFile}
+                    />
+                    <label htmlFor="cvFile">
+                      {valueFile?.target?.files[0]?.name || (
+                        <>
+                          <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            className="iborrainputfile"
+                            width="20"
+                            height="17"
+                            viewBox="0 0 20 17"
+                          >
+                            <path d="M10 0l-5.2 4.9h3.3v5.1h3.8v-5.1h3.3l-5.2-4.9zm9.3 11.5l-3.2-2.1h-2l3.4 2.6h-3.5c-.1 0-.2.1-.2.1l-.8 2.3h-6l-.8-2.2c-.1-.1-.1-.2-.2-.2h-3.6l3.4-2.6h-2l-3.2 2.1c-.4.3-.7 1-.6 1.5l.6 3.1c.1.5.7.9 1.2.9h16.3c.6 0 1.1-.4 1.3-.9l.6-3.1c.1-.5-.2-1.2-.7-1.5z"></path>
+                          </svg>
+                          <span className="iborrainputfile">
+                            {intl.formatMessage({
+                              id: 'app.pages.work-with-us.label.select-file',
+                            })}
+                          </span>
+                        </>
+                      )}
+                    </label>
+                  </div>
+                </div>
+              </div>
+            )}
             <div className="area-text">
               <Label for="exampleText">
                 {intl.formatMessage({
                   id: 'app.pages.work-with-us.label.more',
                 })}
               </Label>
-              <input
+              <textarea
                 className="form-input"
                 id="exampleText"
                 name="moreAboutYou"
